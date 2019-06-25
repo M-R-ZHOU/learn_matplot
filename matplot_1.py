21f6c69a00762cfe658a2a05b4c671605a27b83b
Spurs <2906394727@qq.com>
2019-06-25 14:54:32 +0800

    matplot_finished

--- matplot_1.py
+++ matplot_1.py
@@ -13,7 +13,9 @@ plt.xlim(-3,3)
 plt.legend(handles = (line1,line2,),labels=('line_1','line_2',),loc='best')
 # loc代表图例放置位置的参数，一般有'best' 'upper right' 'upper left' 'lower right'
 plt.xticks(np.linspace(-3,3,21))
+
 plt.yticks([-2,-1.8,-1,1.22,3,],[r'$really\ bad$',r'$bad\ \alpha$',r'$normal$',r'$good$',r'$really\ good$',])
+
 ax = plt.gca()
 ax.spines['right'].set_color('none')
 ax.spines['top'].set_color('none')
@@ -21,6 +23,8 @@ ax.xaxis.set_ticks_position('bottom')
 ax.yaxis.set_ticks_position('left')
 ax.spines['bottom'].set_position(('data',0))
 ax.spines['left'].set_position(('data',0))
+for i in ax.get_yticklabels():
+    print(i)
 # 'data'是参数 还可以设置'outward'与'axes' 'axes'的定位方式是比例
 
 #Annotation
